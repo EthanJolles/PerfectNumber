37c4be9b19f16f3da518990a2d3998e5e22a00e1
Ethan Jolles <85588248+EthanJolles@users.noreply.github.com>
2021-09-14 13:45:34 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+    }
+
+    public static boolean isPerfectNumber(int x)
+    {
+        int comparison = 0;
+        if(x<1)
+        {
+            return false;
+        }
+        {
+            for(int i = 1; i<x; i++)
+            {
+                if (x % i == 0)
+                {
+                    comparison += i;
+                    System.out.println(i);
+                }
+            }
+            return (comparison == x);
+        }
+    }
+}
